026deb6ad67840851452b2522d50982bc5d8c5b0
FabrizioSG <fabriziosg95@gmail.com>
2023-08-05 17:23:39 -0600

    Test con varios casos de prueba

--- test.js
+++ test.js
@@ -20,7 +20,7 @@ const wdOpts = {
     capabilities,
 };
 
-async function runTest() {
+async function runTest1() {
     const driver = await remote(wdOpts);
     try {
         // Localizar y hacer clic en los botones para la suma
@@ -36,7 +36,7 @@ async function runTest() {
         const buttonEquals = await driver.$('//android.widget.ImageButton[@content-desc="equals"]');
         await buttonEquals.click();
 
-        await driver.pause(1000);
+        await driver.pause(1000); 
 
         // Localizar el elemento que contiene el texto de la pantalla de la calculadora
         const screenElement = await driver.$('/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout/android.widget.FrameLayout[1]/android.widget.TextView');
@@ -51,5 +51,52 @@ async function runTest() {
         await driver.deleteSession();
     }
 }
+/*
+Este runtest2 es solo para probar correr varios casos de prueba seguidos
+Hay que escoger cuales otros 2 casos de prueba queremos
+El 2 tiene que pasar y un tercero que falle
+*/
+async function runTest2() {
+    const driver = await remote(wdOpts);
+    try {
+        // Localizar y hacer clic en los botones para la suma
+        const buttonOne = await driver.$('//android.widget.ImageButton[@content-desc="2"]');
+        await buttonOne.click();
 
-runTest().catch(console.error);
+        const buttonPlus = await driver.$('//android.widget.ImageButton[@content-desc="plus"]');
+        await buttonPlus.click();
+
+        const buttonThree = await driver.$('//android.widget.ImageButton[@content-desc="3"]');
+        await buttonThree.click();
+
+        const buttonEquals = await driver.$('//android.widget.ImageButton[@content-desc="equals"]');
+        await buttonEquals.click();
+
+        await driver.pause(1000); 
+
+        // Localizar el elemento que contiene el texto de la pantalla de la calculadora
+        const screenElement = await driver.$('/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout/android.widget.FrameLayout[1]/android.widget.TextView');
+
+        // Obtener el texto de la pantalla
+        const text = await screenElement.getText();
+        // Hacer la aserción para verificar si el resultado es "4"
+        assert.strictEqual(text, "5", `El resultado esperado es "5", pero se obtuvo "${text}"`);
+        
+        console.log('Resultado de la suma:', text);
+    } finally {
+        await driver.deleteSession();
+    }
+}
+// Ejecutar los casos de prueba
+async function runAllTests() {
+    try {
+      await runTest1();
+      await runTest2();
+    } catch (error) {
+      console.error('Error en las pruebas:', error);
+    }
+  }
+  
+  // Llamar a la función para ejecutar todos los casos de prueba
+runAllTests();
+
